000fb2c24bf6dce22312e36b3cc375ac9a3efa2a
MarcoBione <marcobione.work@gmail.com>
2023-06-13 18:04:22 +0200

    Fix axios get w/Marco + add cards in template App

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,19 @@
     <div class="container border border-2">
       <div class="row">
         <div class="col">
-          <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+
+          <div class="card" v-for="project in projects">
+
+            <img :src="project.image" class="card-img-top" alt="...">
+
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
+              <h5 class="card-title">{{ project.name }}</h5>
+              <p class="card-text">//</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
+
           </div>
+
         </div>
       </div>
     </div>
@@ -25,15 +29,20 @@ import axios from 'axios';
 export default {
   name: 'App',
   data() {
-    projects: [];
-    apiBaseUrl: 'http://127.0.0.1:8000/api/';
-    endPointProjects: 'projects';
+    return {
+      projects: [],
+      apiBaseUrl: 'http://127.0.0.1:8000/api/',
+      endPointProjects: 'projects',
+    }
+
   },
 
   methods: {
     getProjects() {
-      axios.get(this.apiBaseUrl + this.endPointProjects).then((res) => {
-        console.log(res);
+      axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
+        this.projects = res.data.results.data;
+        console.log('projects : ', this.projects);
+        console.log(res.data.results.data);
       })
     }
   },
