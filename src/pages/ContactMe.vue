b6906b7fa2a1a6e3fec03d4ad61d92036edb883f
MarcoBione <marcobione.work@gmail.com>
2023-06-19 12:26:43 +0200

    Try to add contact page

--- src/pages/ContactMe.vue
+++ src/pages/ContactMe.vue
@@ -1,12 +1,51 @@
 <template>
-    <div>
-        <h1>Contact </h1>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="address" class="form-label text-white-50">Email address</label>
+                <input type="email" name="email" v-model="email" class="form-control" id="address"
+                    aria-describedby="emailHelp">
+                <div id="address" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="body" class="form-label text-white-50">Title</label>
+                <input type="text" name="body" v-model="body" class="form-control" id="body">
+            </div>
+            <div class="mb-3">
+                <label class="form-check-label text-white-50" for="exampleCheck1">Add text</label>
+                <textarea type="text" class="form-check-input " id="exampleCheck1"></textarea>
+            </div>
+            <button type="submit" class="btn btn-secondary">Submit</button>
+        </form>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { store } from '../data/store';
 export default {
-    name: 'ContactMe'
+    name: 'ContactMe',
+
+    data() {
+        return {
+            address: "",
+            name: "",
+            body: "",
+        }
+    },
+    methods: {
+        sendForm() {
+            const data = {
+                address: this.address,
+                name: this.name,
+                body: this.body
+            };
+            axios.post(`${store.apiUrl}/contacts`, data).then((res) => {
+                console.log(res.data);
+            });
+        }
+    }
+
 }
 </script>
 
